dc6055473981005b1a2ea96f4ab6e37c4ee6dca9
Angel Oluwaseun Balogun <angel123@meta.com>
2025-06-13 17:04:45 -0700

    finished

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,12 +1,19 @@
-import {useState} from 'react';
-import { FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import { faEye, faEyeSlash} from '@fortawesome/free-solid-svg-icons';
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons";
 
-
-function MovieCard({ title, posterPath, voteAverage, onClick,isWatched,toggleWatched, toggleFavorite,isFavorited }) {
+function MovieCard({
+  title,
+  posterPath,
+  voteAverage,
+  onClick,
+  isWatched,
+  toggleWatched,
+  toggleFavorite,
+  isFavorited,
+}) {
   const posterUrl = `https://image.tmdb.org/t/p/w500${posterPath}`;
 
- 
   return (
     <div className="movie-card" onClick={onClick}>
       <img src={posterUrl} alt={title} />
@@ -14,21 +21,27 @@ function MovieCard({ title, posterPath, voteAverage, onClick,isWatched,toggleWat
       <p>⭐ {voteAverage}</p>
 
       <div className="movie-card-buttons">
-        <button className="icon_btn eye-btn" onClick={(e) => { e.stopPropagation(); toggleWatched(); }}>
+        <button
+          className="icon_btn eye-btn"
+          onClick={(e) => {
+            e.stopPropagation();
+            toggleWatched();
+          }}
+          style={{backgroundColor:'rgba(0,0,0,0)', color: 'black'}}
+        >
           <FontAwesomeIcon icon={isWatched ? faEye : faEyeSlash} />
         </button>
 
         <button
-          className={`icon_btn heart-btn ${isFavorited ? 'favorited' : ''}`}
+          className={`icon_btn heart-btn ${isFavorited ? "favorited" : ""}`}
           onClick={(e) => {
             e.stopPropagation();
             toggleFavorite();
           }}
         >
-          {isFavorited ? '❤️' : '🤍'}
+          {isFavorited ? "❤️" : "🤍"}
         </button>
       </div>
-      
     </div>
   );
 }
